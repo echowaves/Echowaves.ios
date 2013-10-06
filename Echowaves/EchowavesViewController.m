715f89e0c707fea2d4fd8a07c3b92f4f49d6e091
dmitryame <dmitry@custommode.com>
2013-10-06 13:42:29 -0400

    logic for sending verifying user name password

--- Echowaves/EchowavesViewController.m
+++ Echowaves/EchowavesViewController.m
@@ -17,12 +17,53 @@
 
 
 - (IBAction)startWaving:(UIButton *)sender {
-    NSLog(@" wave name %@ , wave password %@", _waveName.text, _wavePassword.text);
+    // perform a basic validation, wave/password non balnk and exist in the server side, and enter a sending loop
+    
+    
+    // Setup request
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    [request setURL:[NSURL URLWithString:@"http://echowaves.com/login"]];
+    [request setHTTPMethod:@"POST"];
+    
+    // set headers
+    NSString *contentType = [NSString stringWithFormat:@"text/plain"];
+    [request addValue:contentType forHTTPHeaderField:@"Content-Type"];
+    
+    // setup post string
+    NSMutableString *postString = [[NSMutableString alloc] init];
+    [postString appendFormat:@"name=%@", _waveName.text];
+    [postString appendFormat:@"&pass=%@", _wavePassword.text];
+    
+    // I've been told not to use setHTTPBody for post variables, but how else do you do it?
+    [request setHTTPBody:[postString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    // get response
+    NSHTTPURLResponse *urlResponse = nil;
+//    NSError *error = [[NSError alloc] init];
+    
+//    NSData *responseData = [NSURLConnection sendSynchronousRequest:request
+//                                                 returningResponse:&urlResponse
+//                                                             error:&error];
+    
+//    NSString *result = [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding];
+    
+    
+    NSLog(@"Response code: %d", [urlResponse statusCode]);
+  if ([urlResponse statusCode] >=200 && [urlResponse statusCode] <300)
+    {
+//        NSLog(@"Response ==> %@", result);
+        NSLog(@"user name/password found");
+    } else {
+        NSLog(@"user name/password not found");
+    }
+
+    
+    NSLog(@" wave name %@ ", _waveName.text);
     [_waveName setEnabled:NO];
     [_wavePassword setEnabled:NO];
     [sender setEnabled:NO];
     [sender setTitle:@"Currently Waving" forState:UIControlStateNormal];
-    NSLog(@"button should be disabled now");
+
 
 }
 
