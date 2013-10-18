8ca10dceaf50892dc234ebded11dff83f100f35a
dmitryame <dmitry@custommode.com>
2013-10-18 17:48:26 -0400

    reachability manager added

--- Echowaves/EchowavesViewController.m
+++ Echowaves/EchowavesViewController.m
@@ -32,6 +32,8 @@ int imageCount = 0;
     
     // perform authentication, wave/password non blank and exist in the server side, and enter a sending loop
     manager = [AFHTTPRequestOperationManager manager];
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    
     //ideally not going to need the following line, if making a request to json service
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     NSDictionary *parameters = @{@"name": _waveName.text,
@@ -53,12 +55,10 @@ int imageCount = 0;
             [sender setTitle:[NSString stringWithFormat:@"Currently waving %@", _waveName.text] forState:UIControlStateNormal];
             //let's remember when we started the app, from now on -- send all the pictures
             lastCheckTime = [NSDate date];
-            [_appStatus setText:[NSString stringWithFormat:@"successfully signed in"]];
-
+            
         } else {
             // a wrong login, sign in again
             NSLog(@"wrong login, try again");
-            [_appStatus setText:[NSString stringWithFormat:@"wrong wave/password, try again"]];
         }
         
         
@@ -72,92 +72,95 @@ int imageCount = 0;
 
 - (BOOL) postLastImages
 {
-    __block BOOL imageFound = NO;
-    //http://iphonedevsdk.com/forum/iphone-sdk-development/94700-directly-access-latest-photo-from-saved-photos-camera-roll.html
-    ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
-    // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
-    [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
-        // Within the group enumeration block, filter to enumerate just videos.
-        [group setAssetsFilter:[ALAssetsFilter allPhotos]];
-        
-        // iterating over all assets
-        [group enumerateAssetsUsingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
-            __block BOOL imageFound = NO;
-            // The end of the enumeration is signaled by asset == nil.
-            if (alAsset)
-            {
-                NSDate *currentAssetDateTime = [alAsset valueForProperty:ALAssetPropertyDate];
-                
-                NSTimeInterval timeSinceLastPost =
-                [currentAssetDateTime timeIntervalSinceDate:lastCheckTime]; // diff
-                
-                if(timeSinceLastPost > 0.0) {//this means, found an image that was not posted
-                    imageFound = YES;
-                    NSLog(@"found image that was posted %f seconds since last check", timeSinceLastPost);
-
-                    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-                    [formatter setDateFormat:@"yyyyMMddHHmmss"];
-                    NSString *dateString = [formatter stringFromDate:currentAssetDateTime];
+    if([[AFNetworkReachabilityManager sharedManager] accessibilityActivate]) {
+        __block BOOL imageFound = NO;
+        //http://iphonedevsdk.com/forum/iphone-sdk-development/94700-directly-access-latest-photo-from-saved-photos-camera-roll.html
+        ALAssetsLibrary *library = [[ALAssetsLibrary alloc] init];
+        // Enumerate just the photos and videos group by using ALAssetsGroupSavedPhotos.
+        [library enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+            // Within the group enumeration block, filter to enumerate just videos.
+            [group setAssetsFilter:[ALAssetsFilter allPhotos]];
+            
+            // iterating over all assets
+            [group enumerateAssetsUsingBlock:^(ALAsset *alAsset, NSUInteger index, BOOL *innerStop) {
+                __block BOOL imageFound = NO;
+                // The end of the enumeration is signaled by asset == nil.
+                if (alAsset)
+                {
+                    NSDate *currentAssetDateTime = [alAsset valueForProperty:ALAssetPropertyDate];
                     
-                    NSDictionary *parameters = @{@"name": _waveName.text};//,
+                    NSTimeInterval timeSinceLastPost =
+                    [currentAssetDateTime timeIntervalSinceDate:lastCheckTime]; // diff
                     
-                    ALAssetRepresentation *representation = [alAsset defaultRepresentation];
-                    
-                    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    // post image to echowaves.com
-                    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                    UIImageOrientation orientation = UIImageOrientationUp;
-                    NSNumber* orientationValue = [alAsset valueForProperty:@"ALAssetPropertyOrientation"];
-                    if (orientationValue != nil) {
-                        orientation = [orientationValue intValue];
-                    }
-                    
-                    UIImage* orientedImage = [UIImage imageWithCGImage:[representation fullResolutionImage]
-                                                         scale:1.0 orientation:orientation];
-                    
-                    CGSize newSize = orientedImage.size;
-                    newSize.height = newSize.height / 3.0;
-                    newSize.width = newSize.width / 3.0;
-                    
-                    UIGraphicsBeginImageContext( newSize );// a CGSize that has the size you want
-                    [orientedImage drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
-                    //image is the original UIImage
-                    UIImage* resizedImage = UIGraphicsGetImageFromCurrentImageContext();
-                    UIGraphicsEndImageContext();
-
-                    
-                    
-                    NSData *webUploadData=UIImageJPEGRepresentation(resizedImage, 1.0);
-                    [_appStatus setText:[NSString stringWithFormat:@"uploading image %d", imageCount]];
-
-                    [manager POST:[NSString stringWithFormat:@"%@/upload", host] parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
-                        [formData appendPartWithFileData:webUploadData name:@"file" fileName:[NSString stringWithFormat:@"%@.jpg", dateString] mimeType:@"image/jpeg"];
+                    if(timeSinceLastPost > 0.0) {//this means, found an image that was not posted
+                        imageFound = YES;
+                        NSLog(@"found image that was posted %f seconds since last check", timeSinceLastPost);
                         
-                    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                        //reset the date here
-                        lastCheckTime = currentAssetDateTime;
-                        [_appStatus setText:[NSString stringWithFormat:@"finished uploading image %d", imageCount++]];
-                        NSLog(@"Success posting image");
-                    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                        [_appStatus setText:[NSString stringWithFormat:@"error uploading"]];
-                        NSLog(@"Error posting image: %@", error);
-                    }];
+                        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+                        [formatter setDateFormat:@"yyyyMMddHHmmss"];
+                        NSString *dateString = [formatter stringFromDate:currentAssetDateTime];
+                        
+                        NSDictionary *parameters = @{@"name": _waveName.text};//,
+                        
+                        ALAssetRepresentation *representation = [alAsset defaultRepresentation];
+                        
+                        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        // post image to echowaves.com
+                        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                        UIImageOrientation orientation = UIImageOrientationUp;
+                        NSNumber* orientationValue = [alAsset valueForProperty:@"ALAssetPropertyOrientation"];
+                        if (orientationValue != nil) {
+                            orientation = [orientationValue intValue];
+                        }
+                        
+                        UIImage* orientedImage = [UIImage imageWithCGImage:[representation fullResolutionImage]
+                                                                     scale:1.0 orientation:orientation];
+                        
+                        CGSize newSize = orientedImage.size;
+                        newSize.height = newSize.height / 3.0;
+                        newSize.width = newSize.width / 3.0;
+                        
+                        UIGraphicsBeginImageContext( newSize );// a CGSize that has the size you want
+                        [orientedImage drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+                        //image is the original UIImage
+                        UIImage* resizedImage = UIGraphicsGetImageFromCurrentImageContext();
+                        UIGraphicsEndImageContext();
+                        
+                        
+                        
+                        NSData *webUploadData=UIImageJPEGRepresentation(resizedImage, 1.0);
+                        [_appStatus setText:[NSString stringWithFormat:@"uploading image %d", imageCount]];
+                        
+                        [manager POST:[NSString stringWithFormat:@"%@/upload", host] parameters:parameters constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+                            [formData appendPartWithFileData:webUploadData name:@"file" fileName:[NSString stringWithFormat:@"%@.jpg", dateString] mimeType:@"image/jpeg"];
+                            
+                        } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                            //reset the date here
+                            lastCheckTime = currentAssetDateTime;
+                            [_appStatus setText:[NSString stringWithFormat:@"finished uploading image %d", imageCount++]];
+                            NSLog(@"Success posting image");
+                        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                            [_appStatus setText:[NSString stringWithFormat:@"error uploading"]];
+                            NSLog(@"Error posting image: %@", error);
+                        }];
+                        
+                    } // if timeSinceLastPost
                     
-                } // if timeSinceLastPost
-                
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-                //                                         NSLog(@"image %@", latestPhoto.description);
-                //                                         NSLog(@"asset %@", alAsset.description);
-            }
-        }];
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                    //                                         NSLog(@"image %@", latestPhoto.description);
+                    //                                         NSLog(@"asset %@", alAsset.description);
+                }
+            }];
+        }
+                             failureBlock: ^(NSError *error) {
+                                 // Typically you should handle an error more gracefully than this.
+                                 NSLog(@"No groups. %@", error);
+                             }];
+        return imageFound;
     }
-                         failureBlock: ^(NSError *error) {
-                             // Typically you should handle an error more gracefully than this.
-                             NSLog(@"No groups. %@", error);
-                         }];
-    return imageFound;
+    return NO;
 }
 
 @end
