42021b011b26aa615cf0952997af848b4cadaedf
dmitryame <dmitry@custommode.com>
2013-10-18 16:20:12 -0400

    ahhh, works!

--- Echowaves/EchowavesAppDelegate.m
+++ Echowaves/EchowavesAppDelegate.m
@@ -96,9 +96,9 @@
     NSLog(@"called applicationDidBecomeActive");
 
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    EchowavesViewController* echowavesViewController = (EchowavesViewController*)  self.window.rootViewController;
-    
-    [echowavesViewController postLastImages];
+//    EchowavesViewController* echowavesViewController = (EchowavesViewController*)  self.window.rootViewController;
+//    
+//    [echowavesViewController postLastImages];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
