a1b7cade07bca0b73a5b6834bd51b323b6d79314
dmitryame <dmitry@custommode.com>
2014-01-25 11:27:14 -0500

    added under construction image

--- Echowaves/EchowavesAppDelegate.h
+++ Echowaves/EchowavesAppDelegate.h
@@ -13,3 +13,8 @@
 @interface EchowavesAppDelegate : UIResponder <UIApplicationDelegate>
 @property (strong, nonatomic) UIWindow *window;
 @end
+
+
+//http://all-free-download.com/free-vector/vector-web-design/under_construction_template_241387_download.html
+//https://www.iconfinder.com/free_icons
+//https://www.iconfinder.com/search/?q=iconset%3Awpzoom-developer-icon-set
