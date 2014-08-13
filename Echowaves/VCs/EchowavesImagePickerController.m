35815051cb0d68532588a370459090520674aeb5
dmitryame <dmitry@custommode.com>
2014-08-13 17:24:07 -0400

    WIP: UX improvements (do not reset time when taking photo from the app)

--- Echowaves/VCs/EchowavesImagePickerController.m
+++ Echowaves/VCs/EchowavesImagePickerController.m
@@ -51,7 +51,7 @@
     
     //adjust waving to on
 //    [APP_DELEGATE.wavingViewController.waving setOn:TRUE];
-    [USER_DEFAULTS setObject:[NSDate date] forKey:@"lastCheckTime"];
+//    [USER_DEFAULTS setObject:[NSDate date] forKey:@"lastCheckTime"];
 //    [appDelegate.wavingViewController appStatus].text = @"Use iPhone cam, then come back to EW...";
 
 //    [USER_DEFAULTS setBool:APP_DELEGATE.wavingViewController.waving.on forKey:@"waving"];
