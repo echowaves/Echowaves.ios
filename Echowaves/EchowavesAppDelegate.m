978f456451aa8c8bf2eb489379d841edfe26ebef
dmitryame <dmitry@custommode.com>
2014-08-28 15:56:19 -0400

    removing few deprecations

--- Echowaves/EchowavesAppDelegate.m
+++ Echowaves/EchowavesAppDelegate.m
@@ -68,7 +68,7 @@
     //Set bar appearance
     [[UINavigationBar appearance] setBarTintColor:UIColorFromRGB(0xFFA500)];
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
-    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], UITextAttributeTextColor, [UIColor whiteColor], UITextAttributeTextShadowColor, nil]];
+    [[UINavigationBar appearance] setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, [UIColor whiteColor], NSForegroundColorAttributeName, nil]];
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     
     return YES;
